057a856addeca01ec06bcbaa9478c2c77b9dde4e
Fatima Yousif <fyousif30@gmail.com>
2021-08-15 14:51:17 +0500

    Conditions applied

--- app/src/main/java/com/example/broadcast_receiver/MainActivity.java
+++ app/src/main/java/com/example/broadcast_receiver/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.broadcast_receiver;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_PERMISSION_REQUEST_RECEIVE_SMS=0;
+
+    BroadcastReceiver broadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        broadcastReceiver=new BroadcastReceiver();
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS)!= PackageManager.PERMISSION_GRANTED)
+        {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECEIVE_SMS))
+            {
+                //do nth- as DENIED
+            }
+            else
+            {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_SMS}, MY_PERMISSION_REQUEST_RECEIVE_SMS);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull @org.jetbrains.annotations.NotNull String[] permissions, @NonNull @org.jetbrains.annotations.NotNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSION_REQUEST_RECEIVE_SMS:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "THANK YOU FOR PERMITTING", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "PERMISSION DENIED.. SORRY", Toast.LENGTH_LONG).show();
+                }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
